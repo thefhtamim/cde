a1329d12511c9e4de1faec40d37800db293c563d
thefhtamim <73883619+thefhtamim@users.noreply.github.com>
2021-05-22 11:05:14 +0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -90,8 +90,7 @@ let footer = $(`
   <div class="container p-4">
     <div class="row">
       <div class="col-lg-6 col-md-12 mb-4 mb-md-0 justify-content-center">
-        <p class="tag" >"Imagination is everything. It is the preview of life's coming attractions-Albert Einstein
-."
+        <p class="tag" >"Imagination is everything. It is the preview of life's coming attractions."-Albert Einstein
         </p>
           <div class="rounded-social-buttons tag">
             
